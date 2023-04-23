a1bdee9a600c4f06233d08a66ae04690fcd6de0a
chartoevsky <chartoevsky@gmail.com>
2023-04-23 19:42:11 +0300

    Here's the new ABtasty project on Cypress

---
+++ cypress/e2e/firstTest.cy.js
@@ -0,0 +1,21 @@
+
+
+it ('By id', ()=> {
+    cy.visit ('https://app2.abtasty.com/login')
+    cy.get('#email')
+})
+
+it ('By Class', ()=> {
+    cy.visit ('https://app2.abtasty.com/login')
+    cy.get('._commonInput_v5fef_184')
+})
+
+it ('By Tag', ()=> {
+    cy.visit ('https://app2.abtasty.com/login')
+    cy.get('._commonInput_v5fef_184')
+})
+
+it.only ('By Value', ()=> {
+    cy.visit ('https://app2.abtasty.com/login')
+    cy.get('._commonInput_v5fef_184')
+})
